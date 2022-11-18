2d1bb25aa4160d3d46d1a7e1b705bba8777531ab
sanyaswb <sanyaswb@gmail.com>
2022-11-18 05:09:55 +0200

    completed 26_sum-of-array

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumOfArray(arr) {
-	// write your code here
+	let result = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		result += arr[i];
+	}
+
+	return result;
 }
 
 module.exports = sumOfArray;
